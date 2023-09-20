087ef51a36382a33b288af64387b4fc704197095
1langit <langitlintangradjendra@mail.ugm.ac.id>
2023-09-20 22:44:25 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="mb-4">Data Buku</h1>
+        <table class="table table-striped table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th>id</th>
+                    <th>Judul buku</th>
+                    <th>Penulis</th>
+                    <th>Tgl. Terbit</th>
+                    <th>Harga</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data_buku as $buku)
+                    <tr>
+                        <td>{{ ++$no }}</td>
+                        <td>{{ $buku->judul }}</td>
+                        <td>{{ $buku->penulis }}</td>
+                        <td>{{ date('d/m/y', strtotime($buku->tgl_terbit)) }}</td>
+                        <td>{{ "Rp".number_format($buku->harga, 2, ',', '.') }}</td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <td>Jumlah data: {{ $jumlah_data }} buku</td>
+                    <td>Total harga: {{ "Rp".number_format($total_harga, 2, ',', '.') }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
